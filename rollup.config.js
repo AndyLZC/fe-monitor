af1a35ac0221105c0e04102a848843a8e7dcf9cc
zc l <andylzce@gmail.com>
2023-02-09 20:14:02 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import esbuild from 'rollup-plugin-esbuild'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+const plugins = [
+    nodeResolve(),
+    esbuild({
+        // All options are optional
+        include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+        exclude: /node_modules/, // default
+        sourceMap: true, // default
+        minify: process.env.NODE_ENV === 'production',
+        target: 'esnext', // default, or 'es20XX', 'esnext'
+        jsx: 'transform', // default, or 'preserve'
+        jsxFactory: 'React.createElement',
+        jsxFragment: 'React.Fragment',
+        // Like @rollup/plugin-replace
+        define: {
+          __VERSION__: '"x.y.z"',
+        },
+        tsconfig: 'tsconfig.json', // default
+        // Add extra loaders
+        loaders: {
+          // Add .json files support
+          // require @rollup/plugin-commonjs
+          '.json': 'json',
+          // Enable JSX in .js files too
+          '.js': 'jsx',
+        },
+      }),
+]
+
+export default {
+  input: './src/B.ts',
+  output:  {
+    file: 'dist/index.iife.js',
+    format: 'iife',
+    name: 'monitor',
+    sourcemap: 'true',
+  },
+  plugins
+}
